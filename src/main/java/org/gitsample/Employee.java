a67da4608703dbd89f81aeb9718f4ccfe90836b1
Shirish Gatadi <gatadishirish@gmail.com>
2022-01-06 15:41:00 +0530

    Adding salary field in employee - master

--- src/main/java/org/gitsample/Employee.java
+++ src/main/java/org/gitsample/Employee.java
@@ -3,6 +3,14 @@ package org.gitsample;
 public class Employee {
 	private String name;
 	private String address ;
+	private double salary;
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	
 	
 	
 	
